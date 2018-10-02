1277ca673f8941677fe4413def38bba209622505
Alexandr <34417026+KamenkoAlexandr@users.noreply.github.com>
2018-10-03 01:37:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ function moveBoy(e){
 
     switch(e.keyCode){
 
-        case 37:  // если нажата клавиша влево
+        case 37:  
             if(left > 21){
                 circle.style.marginLeft = left - 70 + "px";
                 boy.src = './images/boy-left.png';
             }
             break;
-        case 38:   // если нажата клавиша вверх
+        case 38:   
             if(top > 35){
                 circle.style.marginTop = top - 70 + "px";
                 boy.src = './images/boy-back.png';
             }
             break;
-        case 39:   // если нажата клавиша вправо
+        case 39:   
             if( left < 380){
                 circle.style.marginLeft = left + 70 + "px";
                 boy.src = './images/boy-right.png';
             }
             break;
-        case 40:   // если нажата клавиша вниз
+        case 40:  
             if(top < 380){
                 circle.style.marginTop = top + 70 + "px";
                 boy.src = './images/boy-front.png';
